12ebd6d005ac7dfd2ea85c6617d3f1a7b49f7e33
Amaury Jr <121615990+amaurysarmento@users.noreply.github.com>
2023-12-18 09:12:13 -0300

    Create calculadoraDeHoras.py
    Trabalhando para saber o resultado só da hora.

---
+++ calculadoraDeHoras.py
@@ -0,0 +1,13 @@
+print('Voce deve colocar valores entre 0 e 23.59')
+
+horaInicial = float(input('Digite a hora inicial:\n'))
+#minutoInicial = float(input('Digite o minuto inicial:\n'))
+
+horaFinal = float(input('Digite a hora final:\n'))
+#minutoFinal = float(input('Digite o minuto final:\n'))
+
+def calcularHora():
+   resultHora = (horaInicial - horaFinal) - 24  
+   print(resultHora)
+
+calcularHora()
