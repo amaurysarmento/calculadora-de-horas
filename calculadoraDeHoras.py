83ade8e3aef8842f23136122b5f11b77e4fd5011
Amaury Jr <121615990+amaurysarmento@users.noreply.github.com>
2023-12-18 11:17:46 -0300

    Update calculadoraDeHoras.py

--- calculadoraDeHoras.py
+++ calculadoraDeHoras.py
@@ -1,13 +1,22 @@
-print('Voce deve colocar valores entre 0 e 23.59')
+import datetime
 
-horaInicial = float(input('Digite a hora inicial:\n'))
-#minutoInicial = float(input('Digite o minuto inicial:\n'))
 
-horaFinal = float(input('Digite a hora final:\n'))
-#minutoFinal = float(input('Digite o minuto final:\n'))
+print('Voce deve colocar valores entre 0 e 23')
 
-def calcularHora():
-   resultHora = (horaInicial - horaFinal) 
-   print(resultHora)
+horaInicial = int(input('Digite a hora inicial:\n'))
+minutoInicial = int(input('Digite o minuto inicial:\n'))
 
-calcularHora()
+horaFinal = int(input('Digite a hora final:\n'))
+minutoFinal = int(input('Digite o minuto final:\n'))
+
+calcRes = datetime.time(horaInicial, minutoInicial)
+calcRes1 = datetime.time(horaFinal, minutoFinal)
+
+
+
+
+ 
+def calcularHora(calcRes, calcRes1):
+    ... #ta indicando que vai ser feita alguma coisa nesse bloco de código.
+
+calcularHora(horaInicial,horaFinal) #Aqui to fazendo a funcao receber os valores das hrs pelo argumento.
