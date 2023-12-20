c973757668d681cb0afc288789e34139277ed927
Amaury Jr <121615990+amaurysarmento@users.noreply.github.com>
2023-12-20 15:14:06 -0300

    Update calculadoraDeHoras.py
    Adicionando import datetime e timedelta

--- calculadoraDeHoras.py
+++ calculadoraDeHoras.py
@@ -1,22 +1,29 @@
 import datetime
-
+from datetime import datetime, timedelta
 
 print('Voce deve colocar valores entre 0 e 23')
 
-horaInicial = int(input('Digite a hora inicial:\n'))
-minutoInicial = int(input('Digite o minuto inicial:\n'))
+horaInicial = int (input('Digite a hora inicial:\n'))
+minutoInicial = int (input('Digite o minuto inicial:\n'))
 
-horaFinal = int(input('Digite a hora final:\n'))
-minutoFinal = int(input('Digite o minuto final:\n'))
+horaFinal = int (input('Digite a hora final:\n'))
+minutoFinal = int (input('Digite o minuto final:\n'))
 
 calcRes = datetime.time(horaInicial, minutoInicial)
 calcRes1 = datetime.time(horaFinal, minutoFinal)
 
+print(calcRes, calcRes1)
+
+res = calcRes - calcRes1
+
+print(res)
 
 
 
- 
-def calcularHora(calcRes, calcRes1):
-    ... #ta indicando que vai ser feita alguma coisa nesse bloco de código.
+# def calcularHora(l1, l2):
+#     hrInit = datetime.now(l1)
+#     hrFin = datetime.now(l2)
+#     res = hrInit - hrFin
+#     print(res)
 
-calcularHora(horaInicial,horaFinal) #Aqui to fazendo a funcao receber os valores das hrs pelo argumento.
+# calcularHora(horaInicial, horaFinal) #Aqui to fazendo a funcao receber os valores das hrs pelo argumento.
