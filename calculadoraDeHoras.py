fb20661a117a458cb24bb0cfdff09981ff04f1dd
Amaury Jr <121615990+amaurysarmento@users.noreply.github.com>
2023-12-28 15:21:28 -0300

    Update calculadoraDeHoras.py

--- calculadoraDeHoras.py
+++ calculadoraDeHoras.py
@@ -1,29 +1,19 @@
-import datetime
-from datetime import datetime, timedelta
-
 print('Voce deve colocar valores entre 0 e 23')
 
 horaInicial = int (input('Digite a hora inicial:\n'))
-minutoInicial = int (input('Digite o minuto inicial:\n'))
+#minutoInicial = input('Digite o minuto inicial:\n')
 
-horaFinal = int (input('Digite a hora final:\n'))
-minutoFinal = int (input('Digite o minuto final:\n'))
+horaFinal =  int (input('Digite a hora final:\n'))
+#minutoFinal = input('Digite o minuto final:\n')
 
-calcRes = datetime.time(horaInicial, minutoInicial)
-calcRes1 = datetime.time(horaFinal, minutoFinal)
-
-print(calcRes, calcRes1)
-
-res = calcRes - calcRes1
-
-print(res)
+#exemplo c/ numero: 22h p/ 8h = 10h e 8h p/ 22h = 14h
+if horaInicial < horaFinal: 
+    #8h p/ 22h = 14h
+    result = horaFinal - horaInicial
+    print(f'{result}h')
 
 
-
-# def calcularHora(l1, l2):
-#     hrInit = datetime.now(l1)
-#     hrFin = datetime.now(l2)
-#     res = hrInit - hrFin
-#     print(res)
-
-# calcularHora(horaInicial, horaFinal) #Aqui to fazendo a funcao receber os valores das hrs pelo argumento.
+if horaInicial > horaFinal: 
+    #22h p/ 8h = 10h
+    result = 24 - (horaInicial - horaFinal)
+    print(f'{result}h')
