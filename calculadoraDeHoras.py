9c9b355e88d27facfe00b004520ae38c43d36219
Amaury Jr <121615990+amaurysarmento@users.noreply.github.com>
2023-12-29 14:36:14 -0300

    Update calculadoraDeHoras.py
    Adicionando a comparação de horas e minutos inicias e finais.

--- calculadoraDeHoras.py
+++ calculadoraDeHoras.py
@@ -1,19 +1,35 @@
 print('Voce deve colocar valores entre 0 e 23')
 
 horaInicial = int (input('Digite a hora inicial:\n'))
-#minutoInicial = input('Digite o minuto inicial:\n')
+minutoInicial = int (input('Digite o minuto inicial:\n'))
 
 horaFinal =  int (input('Digite a hora final:\n'))
-#minutoFinal = input('Digite o minuto final:\n')
+minutoFinal = int (input('Digite o minuto final:\n'))
+
+
 
 #exemplo c/ numero: 22h p/ 8h = 10h e 8h p/ 22h = 14h
-if horaInicial < horaFinal: 
+if horaInicial < horaFinal and minutoInicial < minutoFinal: 
     #8h p/ 22h = 14h
     result = horaFinal - horaInicial
-    print(f'{result}h')
+    resultMnts = minutoFinal - minutoInicial
+    print(f'{result}h{resultMnts}') #Funcionando corretamente com os minutos tambem
 
+if horaInicial < horaFinal and minutoInicial > minutoFinal: 
+    #8h p/ 22h = 14h
+    result = horaFinal - horaInicial - 1 # -1 porque tava 1h a mais.
+    resultMnts = 60 - (minutoInicial - minutoFinal)
+    print(f'{result}h{resultMnts}')#Funcionando corretamente com os minutos tambem
 
-if horaInicial > horaFinal: 
+if horaInicial > horaFinal and minutoInicial < minutoFinal: 
     #22h p/ 8h = 10h
     result = 24 - (horaInicial - horaFinal)
-    print(f'{result}h')
+    resultMnts = minutoFinal - minutoInicial 
+    print(f'{result}h{resultMnts}')#Funcionando corretamente com os minutos tambem
+
+#Não concluida essa parte do algoritmo
+if horaInicial > horaFinal and minutoInicial > minutoFinal: 
+    #22h p/ 8h = 10h
+    result = 24 - (horaInicial - horaFinal) - 1# A hora ta saindo certa.
+    resultMnts = minutoInicial + minutoFinal # Os minutos tão passando pra mais ou pra menos geralmente em 12h
+    print(f'{result}h{resultMnts}')
